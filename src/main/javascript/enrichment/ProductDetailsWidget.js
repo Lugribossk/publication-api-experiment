3a2840f7f1f282f79976da4c84d8fcff11c774fa
Bo Gotthardt <mail@gotthardt.bo>
2013-04-29 22:31:47 +0200

    Added support for widget product sources.

--- src/main/javascript/enrichment/ProductDetailsWidget.js
+++ src/main/javascript/enrichment/ProductDetailsWidget.js
@@ -1,6 +1,7 @@
-define(["enrichment/Enrichment", "internal/Reference", "publication/Product"],
-    function (Enrichment, Reference, Product) {
+define(["enrichment/Enrichment", "internal/Reference", "publication/Product", "util/Logger", "util/Promise"],
+    function (Enrichment, Reference, Product, Logger, Promise) {
         "use strict";
+        var log = new Logger("ProductDetailsWidget");
 
         /**
          * A widget with details about a {@link Product}. Similar to a {@link ProductLink}, but with more advanced settings.
@@ -18,6 +19,8 @@ define(["enrichment/Enrichment", "internal/Reference", "publication/Product"],
             this.button = data.button;
             this.shapeEnabled = data.shapeEnabled;
 
+            this._productSourceType = data.productSourceType;
+            this._externalProduct = data.externalProduct;
             this._productDescriptor = data.productDescriptor;
         }
         ProductDetailsWidget.prototype = Object.create(Enrichment.prototype);
@@ -28,7 +31,16 @@ define(["enrichment/Enrichment", "internal/Reference", "publication/Product"],
          * @return {Promise} A promise for the {@link Product}.
          */
         ProductDetailsWidget.prototype.getProduct = function () {
-            return new Reference(this._productDescriptor).getAs(Product);
+            if (this._productSourceType === ProductDetailsWidget.ProductSource.PRODUCT_DATABASE) {
+                return new Reference(this._productDescriptor).getAs(Product);
+            }
+            if (this._productSourceType === ProductDetailsWidget.ProductSource.EXTERNAL) {
+                return Promise.resolved(new Product({
+                    properties: this._externalProduct
+                }));
+            }
+            log.warn("Unknown product source type ", this._productSourceType);
+            return null;
         };
 
         /**
@@ -38,6 +50,16 @@ define(["enrichment/Enrichment", "internal/Reference", "publication/Product"],
          * @type {String}
          */
         ProductDetailsWidget.TYPE = "productDetailsWidget";
+
+        /**
+         * The types of product sources.
+         * @static
+         * @enum ProductDetailsWidget.ProductSource
+         */
+        ProductDetailsWidget.ProductSource = {
+            PRODUCT_DATABASE: 1,
+            EXTERNAL: 2
+        };
 
         return ProductDetailsWidget;
     });
